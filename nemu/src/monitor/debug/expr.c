dc403e1ac7388b1f61d0f24e3dcf8c235e52bd77
tracer-ics2020 <tracer@njuics.org>
2020-12-19 20:22:52 +0800

    >  compile 20181343107    Static hostname: localhost.localdomain          Icon name: computer-vm            Chassis: vm         Machine ID: cb8876f749ce4cacb0322e83da4dc95f            Boot ID: 817f4735fdf04c4487b689ff0a68ced8     Virtualization: vmware   Operating System: CentOS Linux 7 (Core)        CPE OS Name: cpe:/o:centos:centos:7             Kernel: Linux 3.10.0-1160.6.1.el7.x86_64       Architecture: x86-64  20:22:52 up  4:59,  2 users,  load average: 0.40, 0.23, 0.22

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -29,7 +29,7 @@ static struct rule {
   {"\\*",  '*'},		//mul
   {"\\(",   '(' }, 
   {"\\)",   ')' },
-  {"[0-9]", TK_NUM}
+  {"[[:digit:]]", TK_NUM}
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[14]) )
