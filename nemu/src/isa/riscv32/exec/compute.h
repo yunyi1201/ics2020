9fa5fae02834b5e06d51c0eb753f35649871c5e2
tracer-ics2020 <tracer@njuics.org>
2021-07-21 10:54:57 +0800

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: a38c6a0cd55e408ab2c18d8789fea586     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-59-generic       Architecture: x86-64  10:54:57 up  3:31,  1 user,  load average: 0.45, 0.42, 0.52

--- nemu/src/isa/riscv32/exec/compute.h
+++ nemu/src/isa/riscv32/exec/compute.h
@@ -5,7 +5,7 @@ static inline def_EHelper(lui) {
 
 static inline def_EHelper(addi) {
 	rtl_addi(s, ddest, dsrc1, id_src2->imm);
-	print_asm_template2(addi);
+	print_asm_template3(addi);
 }
 
 static inline def_EHelper(auipc) {	
