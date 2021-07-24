1e00e64404474adf15d9c9421fe39ab8dd595471
tracer-ics2020 <tracer@njuics.org>
2021-07-24 17:02:27 +0800

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  17:02:28 up  1:39,  1 user,  load average: 0.71, 0.89, 0.71

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -4,7 +4,7 @@
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
-
+/*
 static char digits[] = "0123456789ABCDEF";
 
 static size_t printint(char *str, int xx, int base, int sgn) 
@@ -106,4 +106,144 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 int sscanf(const char *str, const char *format, ...){
 	return 0;
 }
+*/
+
+#define MAX_BUF 2048
+static inline size_t iofmt_int(char *dest, int64_t d, uint32_t base) {
+  /* Print all the digits in *dest and
+   * return the length of the integer */
+
+  const char map[] = "0123456789abcdef";
+  size_t len       = 0;
+  bool minus       = false;
+  char buffer[65];
+  memset(buffer, 0, sizeof(buffer));
+  char *buf_ptr = &buffer[0];
+
+  if (d < 0) {
+    minus = true;
+    d     = -d;
+  }
+
+  // Print out the digit in reverse order
+  do {
+    *buf_ptr++ = map[d % base];
+    d /= base;
+  } while (d != 0);
+
+  if (minus) {
+    *buf_ptr++ = '-';
+  }
+
+  len = buf_ptr - buffer;
+
+  // Reverse the buffer
+  while (buf_ptr != buffer) {
+    *dest++ = *(--buf_ptr);
+  }
+
+  return len;
+}
+
+int printf(const char *fmt, ...) {
+  va_list arg;
+  int done = 0;
+
+  /* defined in klib.h, need to be modified.
+   * MAX_BUF >= a certain amount can lead to exceed */
+  char buf[MAX_BUF];
+  memset(buf, 0, sizeof(buf));
+
+  // Passing those arguemnts to sprintf
+  va_start(arg, fmt);
+  done = vsprintf(buf, fmt, arg);
+  va_end(arg);
+
+  int i;
+  for (i = 0; i < done; ++i) {
+    putch(buf[i]);
+  }
+
+  return done;
+}
+
+// TODO: to support longer/limited(vsnprintf) output
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  /* sprintf with va_list argument
+   * %d: int32_t
+   * %s: char*
+   * %c: char
+   * %l(non-standard): int64_t */
+
+  char *done = out;
+  int d;
+  long long l;
+  char c;
+  char *s;
+  void *p;
+
+  while (*fmt) {
+    switch (*fmt) {
+    case '%': {
+      ++fmt;
+      switch (*fmt) {
+      case 'd':
+        d = va_arg(ap, int);
+        out += iofmt_int(out, d, 10);
+        break;
+      case 's':
+        s = va_arg(ap, char *);
+        memcpy(out, s, strlen(s));
+        out += strlen(s);
+        break;
+      case 'c':
+        // Char -> int in argument passing(?)
+        c      = va_arg(ap, int);
+        *out++ = c;
+        break;
+      case 'l':
+        l = va_arg(ap, int64_t);
+        out += iofmt_int(out, l, 10);
+        break;
+      case 'p': {
+        p = va_arg(ap, void *);
+        out += iofmt_int(out, (uint32_t)p, 16);
+        break;
+      }
+      case 'x':
+        l = va_arg(ap, uint32_t);
+        out += iofmt_int(out, l, 16);
+        break;
+      default: {
+        char tmp[] = "<<pattern not implemented>>";
+        memcpy(out, tmp, strlen(tmp));
+        out += strlen(tmp);
+        // assert(0);
+        break;
+      }
+      }
+      break;
+    }
+    default: *out++ = *fmt;
+    }
+    ++fmt;
+  }
+  *out = '\0';
+
+  return out - done;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+  /* sprintf pass those arguments to vsprintf */
+  va_list ap;
+  va_start(ap, fmt);
+  int done = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return done;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) { return 0; }
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) { return 0; }
+
 #endif
