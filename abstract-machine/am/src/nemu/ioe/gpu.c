2faf316132936a0678ac67f99e4b77bfcd8eea3d
tracer-ics2020 <tracer@njuics.org>
2021-07-25 08:29:38 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  08:29:39 up  6:41,  1 user,  load average: 0.52, 0.57, 0.55

--- abstract-machine/am/src/nemu/ioe/gpu.c
+++ abstract-machine/am/src/nemu/ioe/gpu.c
@@ -10,7 +10,7 @@ void __am_gpu_init() {
 	GPU_W = inw(VGACTL_ADDR+2);
 	VMEMSZ = GPU_W * GPU_H * sizeof(uint32_t);
 	uint32_t *fb = (uint32_t*)(uintptr_t)FB_ADDR;
-	for(i=0; i<GPU_H*GPU_W; i++) fb[i] = i;
+	for(i=0; i<GPU_H*GPU_W; i++) fb[i] = 0;
 	outl(SYNC_ADDR, 1);
 }
 
