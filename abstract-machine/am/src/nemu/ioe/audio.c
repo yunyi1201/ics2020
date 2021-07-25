d15024a66fc492c2b89f3f661e29e9b6d661a510
tracer-ics2020 <tracer@njuics.org>
2021-07-25 08:55:06 +0800

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  08:55:06 up  7:07,  1 user,  load average: 0.49, 0.63, 0.62

--- abstract-machine/am/src/nemu/ioe/audio.c
+++ abstract-machine/am/src/nemu/ioe/audio.c
@@ -8,19 +8,77 @@
 #define AUDIO_INIT_ADDR      (AUDIO_ADDR + 0x10)
 #define AUDIO_COUNT_ADDR     (AUDIO_ADDR + 0x14)
 
+#define AUDIO_QUEUE_HEAD_ADDR (AUDIO_SBUF_ADDR + 0x00)
+#define AUDIO_QUEUE_END_ADDR  (AUDIO_SBUF_ADDR + 0x04)
+
+static inline int __push(uint8_t val) {
+  /* More likely a rule to maintain a
+   * certain memory space
+   * --------RULE--------
+   * st  : AUDIO_SBUF_ADDR + 0x00, index;
+   * ed  : AUDIO_SBUF_ADDR + 0x04, index;
+   * size: AUDIO_COUNT_ADDR, uint32_t value;
+   * buffer_data: [*st, *ed)
+   * */
+
+  /* Failed to implement */
+
+  // static uint32_t st = 0;
+  static uint32_t ed = 65536; // For tmp usage
+
+  /* Implement with index 555 */
+  // uint32_t head   = (uint32_t) inl(AUDIO_QUEUE_HEAD_ADDR);
+  uint32_t end    = (uint32_t) inl(AUDIO_QUEUE_END_ADDR);
+  uint32_t count  = (uint32_t) inl(AUDIO_COUNT_ADDR);
+
+  if (count >= (uint32_t) 65536 - 8)
+    return -1;
+
+  outb(AUDIO_SBUF_ADDR + end, val);
+  if (end == ed - 1) {
+    outl(AUDIO_QUEUE_END_ADDR, 8);
+  } else {
+    outl(AUDIO_QUEUE_END_ADDR, end + 1);
+  }
+  outl(AUDIO_COUNT_ADDR, count + 1);
+  return 0;
+}
+
+static inline int __am_get_count() {
+  return inl(AUDIO_COUNT_ADDR);
+}
+
 void __am_audio_init() {
 }
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
-  cfg->present = false;
+  cfg->present = true;
+	cfg->bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
 }
 
+
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
+	outl(AUDIO_INIT_ADDR, 1);
+	outl(AUDIO_FREQ_ADDR, ctrl->freq);
+	outl(AUDIO_CHANNELS_ADDR, ctrl->channels);
+	outl(AUDIO_SAMPLES_ADDR, ctrl->samples);
 }
 
 void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
-  stat->count = 0;
+  stat->count = __am_get_count();
 }
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
+	uint8_t *st, *ed;
+  st = ctl->buf.start;
+  ed = ctl->buf.end;
+  // uint32_t count  = (uint32_t) inl(AUDIO_COUNT_ADDR);
+
+  /* Wait until there's space to write
+   * Atomic problems... */
+
+  for (; st != ed; ++st) {
+    /* Writeback count */
+    while (__push(*st) == -1);
+  }
 }
--- nemu/src/device/audio.c
+++ nemu/src/device/audio.c
@@ -22,11 +22,95 @@ enum {
 
 static uint8_t *sbuf = NULL;
 static uint32_t *audio_base = NULL;
+IOMap *queue, *audio_map;
+extern IOMap* fetch_mmio_map(paddr_t addr);
+
+static inline int __pop() {
+  /* Return -1 if there's no element to pop */
+  // static uint32_t st = 0;
+  static uint32_t ed = 65536; // STREAM_MAX_BUF_SIZE
+
+  queue = fetch_mmio_map(STREAM_BUF);
+  audio_map = fetch_mmio_map(AUDIO_MMIO);
+
+  // Use 'index' for better programming experience
+  uint32_t head  = (uint32_t) map_read(STREAM_BUF + 0x00, sizeof(uint32_t), queue);
+  uint32_t count = (uint32_t) map_read(AUDIO_MMIO + 0x14, sizeof(uint32_t), audio_map);
+
+  int ret;
+  if (count == 0)
+    return -1;
+
+  // GET corresponding val from buf pointed by head
+  ret = (int) map_read(STREAM_BUF + head, sizeof(uint8_t), queue);
+  if (head == ed - 1) {
+    map_write(STREAM_BUF + 0x00, 8, sizeof(uint32_t), queue);
+  } else {
+    map_write(STREAM_BUF + 0x00, head + 1, sizeof(uint32_t), queue);
+  }
+  map_write(AUDIO_MMIO + 0x14, count - 1, sizeof(uint32_t), audio_map);
+  return ret;
+}
 
 static inline void audio_play(void *userdata, uint8_t *stream, int len) {
+	int nread = len;
+  uint32_t count  = map_read(AUDIO_MMIO + 0x14, sizeof(uint32_t), audio_map);
+  if (count < len) nread = count;
+
+  // -> size chaned
+  int tmp = nread;
+  while (tmp--) {
+    *stream++ = (uint8_t) __pop();
+    // assert(*(stream - 1) != -1);
+  }
+
+  if (len > nread)
+    memset(stream + nread, 0, len - nread);		
+}
+
+void update_audio() {
+  audio_map = fetch_mmio_map(AUDIO_MMIO);
+  uint32_t init = map_read(AUDIO_MMIO + 0x10, sizeof(uint32_t), audio_map);
+  if (init != 1)
+    return;
+
+  // Set INIT to zero
+  map_write(AUDIO_MMIO + 0x10, 0, sizeof(uint32_t), audio_map);
+
+  // Init the queue IOMap for other functions
+  queue = fetch_mmio_map(STREAM_BUF);
+
+  // Set offset
+  uint32_t freq = map_read(AUDIO_MMIO + 0x00, sizeof(uint32_t), audio_map);
+  uint32_t chan = map_read(AUDIO_MMIO + 0x04, sizeof(uint32_t), audio_map);
+  uint32_t samp = map_read(AUDIO_MMIO + 0x08, sizeof(uint32_t), audio_map);
+
+  // Init bufsize
+  map_write(AUDIO_MMIO + 0x0c, STREAM_BUF_MAX_SIZE, sizeof(uint32_t), audio_map);
+
+  // Init buf queue, head -> 8 and end -> 8
+  map_write(STREAM_BUF + 0x00, 8, sizeof(uint32_t), queue);
+  map_write(STREAM_BUF + 0x04, 8, sizeof(uint32_t), queue);
+
+  SDL_AudioSpec s = {};
+
+  s.format   = AUDIO_S16SYS;
+  s.freq     = freq;
+  s.channels = chan;
+  s.samples  = samp;
+  // SDL2 will invoke the callback function
+  s.callback = audio_play;
+  s.userdata = NULL;
+
+  int ret = SDL_InitSubSystem(SDL_INIT_AUDIO);
+  if (ret == 0) {
+    SDL_OpenAudio(&s, NULL);
+    SDL_PauseAudio(0);
+  }
 }
 
 static void audio_io_handler(uint32_t offset, int len, bool is_write) {
+	return ;
 }
 
 void init_audio() {
