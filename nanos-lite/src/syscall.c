ce7760953de6af26fd2fcd2a8822db3db0c8a5c3
tracer-ics2020 <tracer@njuics.org>
2021-07-26 14:05:34 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  14:05:34 up 1 day,  2:27,  1 user,  load average: 0.36, 0.35, 0.35

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -13,8 +13,8 @@ int sys_exit(int status) {
 
 int sys_write(int fd, void* buf, size_t len) {
 
-	Log("sys_write");
-	Log("time: %s, %s", __TIME__, __DATE__);
+	//Log("sys_write");
+	//Log("time: %s, %s", __TIME__, __DATE__);
 	for(int i=0; i<len; i++) 
 		putch(((char*)buf)[i]);
 	return len;
