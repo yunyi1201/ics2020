7866357118b840e4a8e20986c09d27a0a85eb0ba
tracer-ics2020 <tracer@njuics.org>
2021-07-26 11:32:58 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  11:32:59 up 23:55,  1 user,  load average: 0.39, 0.29, 0.25

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,8 +4,10 @@ static Context* do_event(Event e, Context* c) {
   //switch (e.event) {
   //  default: panic("Unhandled event ID = %d", e.event);
  // }
-	if(e.event == EVENT_YIELD)
-		printf("Tirger event ID = %d", e.event);
+	if(e.event == EVENT_YIELD) {
+		yield();
+	}
+		//printf("Tirger event ID = %d", e.event);
 	else if(e.event == EVENT_SYSCALL) {
 		extern void do_syscall(Context*);
 		do_syscall(c);
