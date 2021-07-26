51647f2af01acdf083ed83bbfbce4602324e9974
tracer-ics2020 <tracer@njuics.org>
2021-07-26 13:04:02 +0800

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  13:04:02 up 1 day,  1:26,  1 user,  load average: 0.59, 0.41, 0.24

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,22 +1,22 @@
 #include <common.h>
 #include "syscall.h"
 
-enum { SYS_exit, SYS_yield };
-
 int sys_yield() {
 	yield();
 	return -1;
 }
 
-int sys_exit() {
+int sys_exit(int status) {
 	halt(0);
+	return status;
 }
+
 void do_syscall(Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
 
   switch (a[0]) {
-		case SYS_exit: 	c->GPRx = sys_exit();  break; 
+		case SYS_exit: 	c->GPRx = sys_exit(c->GPR2);  break; 
 		case SYS_yield: c->GPRx = sys_yield(); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
