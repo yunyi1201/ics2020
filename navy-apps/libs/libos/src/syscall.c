c28021ac96de2c2002d032b550905973bb014205
tracer-ics2020 <tracer@njuics.org>
2021-07-26 13:46:02 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  13:46:02 up 1 day,  2:08,  1 user,  load average: 0.53, 0.37, 0.36

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -63,7 +63,7 @@ int _open(const char *path, int flags, mode_t mode) {
 int _write(int fd, void *buf, size_t count) {
   //_exit(SYS_write);
 	_syscall_(SYS_write, fd, buf, count);
-  return 0;
+  return count;
 }
 
 void *_sbrk(intptr_t increment) {
