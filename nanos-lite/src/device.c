ce9ffd9559178397e64441774ae22df958996d11
tracer-ics2020 <tracer@njuics.org>
2021-07-27 17:41:12 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  17:41:12 up 1 day, 18:01,  1 user,  load average: 0.21, 0.35, 0.35

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -9,10 +9,14 @@
 #define NAME(key) \
   [AM_KEY_##key] = #key,
 
+#define KEYDOWN_MASK 0x8000
+#define KBD_ADDR     0xa1000060
 static const char *keyname[256] __attribute__((used)) = {
   [AM_KEY_NONE] = "NONE",
   AM_KEYS(NAME)
 };
+
+static uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
 
 size_t serial_write(const void *buf, size_t offset, size_t len) {
 	for(int i=0; i<len; i++)
@@ -21,7 +25,20 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  return 0;
+	AM_INPUT_KEYBRD_T kbd;
+	uint32_t key = inl(KBD_ADDR);
+	size_t ret;
+	if(key >= KEYDOWN_MASK) {
+		kbd->keydown = 1;
+		kbd->keycode = key ^ KEYDOWN_MASK;
+		ret = sprintf(buf, "%s %s\n", "kd", keyname[kbd.keycode]);
+	} else if(key != 0) {
+		kbd->keydown = 0;
+		kbd->keycode = key;
+		ret = sprintf(buf, "%s %s\n", "ku", keyname[kbd.keycode]);	
+	} else
+		return 0;
+	return 0;
 }
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
