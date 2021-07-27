4a67cc8789f0017f5fccd90bd67d1e7c0a7f3558
tracer-ics2020 <tracer@njuics.org>
2021-07-27 21:57:17 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  21:57:17 up 1 day, 22:17,  1 user,  load average: 0.70, 0.84, 0.63

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -39,6 +39,14 @@ int NDL_PollEvent(char *buf, int len) {
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
+	FILE *f = fopen("/proc/dispinfo", "r");
+  fscanf(f, "WIDTH: %d\nHEIGHT: %d", &screen_w, &screen_h);
+  // printf("%d %d\n", screen_w, screen_h);
+
+  if (*w == 0 && *h == 0) {
+    *w = screen_w;
+    *h = screen_h;
+  }
   if (getenv("NWM_APP")) {
     int fbctl = 4;
     fbdev = 5;
