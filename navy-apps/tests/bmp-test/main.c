299d4b91da92b472f93f45dbc3be421b09914c36
tracer-ics2020 <tracer@njuics.org>
2021-07-27 22:03:50 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  22:03:50 up 1 day, 22:24,  1 user,  load average: 1.00, 0.83, 0.69

--- navy-apps/tests/bmp-test/main.c
+++ navy-apps/tests/bmp-test/main.c
@@ -11,7 +11,7 @@ int main() {
   assert(bmp);
   NDL_OpenCanvas(&w, &h);
 	printf("WIDTH: %d, HEIGHT: %d\n", w, h);
-  NDL_DrawRect(bmp, -40, 5, w, h);
+  NDL_DrawRect(bmp, -60, 5, w, h);
   free(bmp);
   NDL_Quit();
   printf("Test ends! Spinning...\n");
