3324ab7afb0e67645faea58f5181bb1cd16c265c
tracer-ics2020 <tracer@njuics.org>
2021-07-27 14:14:27 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  14:14:27 up 1 day, 14:34,  1 user,  load average: 0.49, 0.41, 0.34

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,22 +16,22 @@ extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
 	Elf_Ehdr ehdr;
 	Elf_Ehdr *elf = &ehdr;
-	//ramdisk_read((void *)elf, 443927, sizeof(Elf_Ehdr));
-	int fd  = fs_open(filename, 0, 0);
-	int ret = fs_read(fd, elf, sizeof(Elf_Ehdr));
-	assert(ret != 0);
+	ramdisk_read((void *)elf, 400143, sizeof(Elf_Ehdr));
+	//int fd  = fs_open(filename, 0, 0);
+	//int ret = fs_read(fd, elf, sizeof(Elf_Ehdr));
+	//assert(ret != 0);
 	assert(*(uint32_t *)elf->e_ident == 0x464c457f);
-	printf("fd: %d\n", fd);
+	//printf("fd: %d\n", fd);
 	uint32_t phdr_offset = elf->e_phoff;
 	printf("phdr num: %d\n", elf->e_phnum);
 	for(int i=0; i<elf->e_phnum; i++) {
 		Elf_Phdr phdr; 
 		Elf_Phdr *elf_phdr = &phdr;
-		size_t ls_offset = fs_lseek(fd, phdr_offset, SEEK_SET);
-		printf("lseek offset :%d\n", ls_offset);
-		ret = fs_read(fd, elf_phdr, sizeof(Elf_Phdr));
-		printf("read return valur: %d\n", ret);
-		//ramdisk_read(elf_phdr, 443927+phdr_offset, sizeof(Elf_Phdr));
+		//size_t ls_offset = fs_lseek(fd, phdr_offset, SEEK_SET);
+		//printf("lseek offset :%d\n", ls_offset);
+		//ret = fs_read(fd, elf_phdr, sizeof(Elf_Phdr));
+		//printf("read return valur: %d\n", ret);
+		ramdisk_read(elf_phdr, 400143+phdr_offset, sizeof(Elf_Phdr));
 		if(elf_phdr->p_type == PT_LOAD) {
 			size_t offset = elf_phdr->p_offset;
 			uintptr_t vaddr = elf_phdr->p_vaddr;
@@ -42,16 +42,16 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 			printf("phdr offset: %d\n", phdr_offset);
 			printf("load file addr = 0x%x\n", vaddr);
 			printf("load memsz :0x%x\n", memsz);
-			//ramdisk_read((void *)vaddr, 443927+offset, memsz);
-			ls_offset = fs_lseek(fd, offset, SEEK_SET);
-			printf("ls_offset :%d\n", ls_offset);
-			fs_read(fd, (void *)vaddr, memsz);
+			ramdisk_read((void *)vaddr, 400143+offset, memsz);
+			//ls_offset = fs_lseek(fd, offset, SEEK_SET);
+			//printf("ls_offset :%d\n", ls_offset);
+			//fs_read(fd, (void *)vaddr, memsz);
 			if(memsz > filesz) 
 				memset((uint8_t *)vaddr + filesz, 0, memsz - filesz);
 		}	
 		phdr_offset += sizeof(Elf_Phdr);
 	}
-	fs_close(fd);
+	//fs_close(fd);
 	printf("entry content: 0x%x\n", *(uint32_t *)elf->e_entry);
   return (uintptr_t)elf->e_entry;
 }
