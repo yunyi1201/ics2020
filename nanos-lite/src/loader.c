0faae1139366e19d14f6c2c6ad33e2ccb975bc72
tracer-ics2020 <tracer@njuics.org>
2021-07-27 13:43:44 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: b1c4d8b54f9a4029bd7c90d4a4ffad94     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  13:43:44 up 1 day, 14:03,  1 user,  load average: 0.28, 0.26, 0.24

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,7 +24,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	printf("fd: %d\n", fd);
 	uint32_t phdr_offset = elf->e_phoff;
 	printf("phdr num: %d\n", elf->e_phnum);
-	printf("phdr offset: %d\n", phdr_offset);
 	for(int i=0; i<elf->e_phnum; i++) {
 		Elf_Phdr phdr; 
 		Elf_Phdr *elf_phdr = &phdr;
@@ -32,6 +31,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 		fs_read(fd, elf_phdr, sizeof(Elf_Phdr));
 		printf("elf_phdr offset :0x%x\n", elf_phdr->p_offset);
 		printf("elf_phdr vaddr  :0x%x\n", elf_phdr->p_vaddr);
+		printf("phdr offset: %d\n", phdr_offset);
 		//ramdisk_read(elf_phdr, phdr_offset, sizeof(Elf_Phdr));
 		if(elf_phdr->p_type == PT_LOAD) {
 			size_t offset = elf_phdr->p_offset;
