48b4a1959a1686b98dc6e701f288480835845c57
tracer-ics2020 <tracer@njuics.org>
2021-07-31 20:14:19 +0800

    >  compile 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: c0de26755f534a8b8a9d9174018f57dc     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  20:14:19 up  3:47,  1 user,  load average: 0.30, 0.24, 0.20

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -31,7 +31,7 @@ static struct rule {
 	{"/"  , '/' },
 	{"\\(", '(' },
 	{"\\)", ')' },
-	{"0x[0-9a-fA-F]", TK_HEX },
+	{"0X[0-9a-fA-F]", TK_HEX },
 	{"\\$(([\\$rsgt]([0-9a-zA-Z])+)|(pc))", TK_REG},
 };
 
