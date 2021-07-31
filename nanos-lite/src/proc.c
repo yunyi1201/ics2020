001ebf36d16914007ea40bfda40ec4c6cdcaef52
tracer-ics2020 <tracer@njuics.org>
2021-07-31 17:46:36 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: c0de26755f534a8b8a9d9174018f57dc     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  17:46:36 up  1:30,  1 user,  load average: 0.42, 0.57, 0.55

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,7 +24,7 @@ void init_proc() {
   switch_boot_pcb();
 	extern void naive_uload(PCB*, const char*);
   Log("Initializing processes...");
-	naive_uload(NULL, "/bin/bmp-test");
+	naive_uload(NULL, "/bin/menu");
 
   // load program here
 
