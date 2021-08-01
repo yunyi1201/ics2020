e77ab083b25578e03218f976b447cc82f13c92d9
tracer-ics2020 <tracer@njuics.org>
2021-08-01 10:13:31 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: c0de26755f534a8b8a9d9174018f57dc     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  10:13:31 up  8:00,  1 user,  load average: 0.29, 0.23, 0.30

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,9 +25,9 @@ void init_proc() {
 	extern void context_kload(PCB *, void *, void *);
 	context_kload(&pcb[0], hello_fun, NULL);
   switch_boot_pcb();
-	//extern void naive_uload(PCB*, const char*);
+	extern void naive_uload(PCB*, const char*);
   Log("Initializing processes...");
-	//naive_uload(NULL, "/bin/nterm");
+	naive_uload(NULL, "/bin/nterm");
   // load program here
 }
 
