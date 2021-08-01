c03efab20bc6a5ee22676aa8a6fbaf167443a3cf
tracer-ics2020 <tracer@njuics.org>
2021-08-01 13:52:17 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: c0de26755f534a8b8a9d9174018f57dc     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  13:52:18 up 10:16,  1 user,  load average: 0.63, 0.64, 0.58

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,7 +26,7 @@ void init_proc() {
 	extern void context_uload(PCB *, const char *);
 	context_kload(&pcb[0], hello_fun, "A");
 
-	context_uload(&pcb[1], "/bin/hello");
+	context_uload(&pcb[1], "/bin/nterm");
 
   switch_boot_pcb();
 	//extern void naive_uload(PCB*, const char*);
