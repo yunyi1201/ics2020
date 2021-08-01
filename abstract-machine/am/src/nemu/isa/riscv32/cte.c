f39c6ddbe4a398393067789351430ef3297d6b05
tracer-ics2020 <tracer@njuics.org>
2021-08-01 10:15:26 +0800

    >  run 191220000    Static hostname: ubuntu          Icon name: computer-vm            Chassis: vm         Machine ID: e5262bef091a4f1398ef34609cc46228            Boot ID: c0de26755f534a8b8a9d9174018f57dc     Virtualization: vmware   Operating System: Ubuntu 20.04.2 LTS             Kernel: Linux 5.8.0-63-generic       Architecture: x86-64  10:15:26 up  8:02,  1 user,  load average: 0.72, 0.38, 0.34

--- abstract-machine/am/src/nemu/isa/riscv32/cte.c
+++ abstract-machine/am/src/nemu/isa/riscv32/cte.c
@@ -18,7 +18,7 @@ Context* __am_irq_handle(Context *c) {
 			ev.event = EVENT_ERROR;
 		}
 
-    c = user_handler(ev, c);      // 切换进程
+  	user_handler(ev, c);      // 切换进程
     assert(c != NULL);
   }
 
